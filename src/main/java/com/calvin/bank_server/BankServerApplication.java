6f73200a703961b3b9ae05585cf20184e90e0eb0
Calvin <calvintallentspam@gmail.com>
2023-05-08 12:26:11 -0700

    Initial commit.

---
+++ src/main/java/com/calvin/bank_server/BankServerApplication.java
@@ -0,0 +1,34 @@
+package com.calvin.bank_server;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+//public class BankServerApplication {
+
+	//public static void main(String[] args) {
+		//SpringApplication.run(BankServerApplication.class, args);
+	//}
+
+//}
+
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BankServerApplication {
+    public static void main(String[] args) {
+      SpringApplication.run(BankServerApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+}
