22a2c1f3e050f18ef6a05d370396c6103dcf034d
Calvin <calvintallentspam@gmail.com>
2023-05-11 12:30:55 -0700

    Work on authentication.

---
+++ src/main/java/com/calvin/bank_server/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.calvin.bank_server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+            .authorizeHttpRequests((req) -> req.requestMatchers("/","/home","/index.html","/signup.html").permitAll().anyRequest().authenticated())
+            .formLogin((form) -> form.loginPage("/login.html").permitAll())
+            .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+        .username("calvin")
+        .password("password")
+        .roles("USER")
+        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+}
